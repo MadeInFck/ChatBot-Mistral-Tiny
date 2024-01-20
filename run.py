f24b3ac8a6aaf371e08d20b88476e3963f40248b
MadeInFck <webmaster@madeinfck.com>
2024-01-19 22:09:10 -0500

    Auth added to gradio

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 import gradio as gr
-
+import os
 from main import iface
+from dotenv import load_dotenv
 
+load_dotenv()
+user = os.environ.get("user")
+password = os.environ.get("password")
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return 'The chatbot is running at /chatbot', 200
 
-app = gr.mount_gradio_app(app, iface, path='/chatbot')
+app = gr.mount_gradio_app(app, iface.launch(auth=(user, password)), path='/chatbot')
